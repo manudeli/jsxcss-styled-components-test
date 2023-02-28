603dd21eea9397a8488659175a930a577531d950
Jonghyeon Ko <manudeli.ko@gmail.com>
2023-03-01 05:05:43 +0900

    test(@jsxcss): styled-components error TypeError: Cannot read properties of undefined (reading 'withConfig')

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@jsxcss/styled-components'],
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
